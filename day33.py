d39a72457ca7f2c66a334ce93920910f6d27acc2
Sakshi Dehankar <sakshidehankar5@gmail.com>
2026-04-22 11:01:20 +0530

    Add files via upload

---
+++ day33.py
@@ -0,0 +1,24 @@
+# Assign values
+a = 10
+b = 3
+
+# Addition
+print("Addition:", a + b)
+
+# Subtraction
+print("Subtraction:", a - b)
+
+# Multiplication
+print("Multiplication:", a * b)
+
+# Division (float result)
+print("Division:", a / b)
+
+# Floor Division (integer result)
+print("Floor Division:", a // b)
+
+# Modulus (remainder)
+print("Modulus:", a % b)
+
+# Exponentiation (power)
+print("Exponentiation:", a ** b)
