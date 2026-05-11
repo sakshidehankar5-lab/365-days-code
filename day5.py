b534e82cdd708506d53a34e0e816513b13a002c6
Sakshi Dehankar <sakshidehankar5@gmail.com>
2026-05-11 21:26:33 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -14,17 +14,21 @@ print("4. Division")
 
 choice = input("Enter choice (1/2/3/4): ")
 
+
 if choice == "1":
     result = num1 + num2
     print("Result:", result)
+
 
 elif choice == "2":
     result = num1 - num2
     print("Result:", result)
 
+
 elif choice == "3":
     result = num1 * num2
     print("Result:", result)
+
 
 elif choice == "4":
     if num2 != 0:
