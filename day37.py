808183d6cef45fad16c705d8822d269cb6987723
Sakshi Dehankar <sakshidehankar5@gmail.com>
2026-05-12 22:26:29 +0530

    Update day37.py

--- day37.py
+++ day37.py
@@ -5,3 +5,23 @@ a, b = 0, 1
 for _ in range(n):
     print(a)
     a, b = b, a + b
+
+
+
+Simple Student Grade System
+name = input("Enter student name: ")
+marks = float(input("Enter marks: "))
+
+if marks >= 90:
+    grade = "A"
+
+elif marks >= 75:
+    grade = "B"
+
+elif marks >= 50:
+    grade = "C"
+
+else:
+    grade = "Fail"
+
+print(f"{name} got grade {grade}")
